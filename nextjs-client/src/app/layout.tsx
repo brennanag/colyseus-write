96ae5ad6865425864a9fb098e39c9a82d263c6a5
Rachel Fennell <rachelfennell@MacBook-Air-2.local>
2025-10-20 14:48:25 -0700

    dark theme

--- nextjs-client/src/app/layout.tsx
+++ nextjs-client/src/app/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="en">
       <body>
         <Providers>{children}</Providers>
       </body>
--- nextjs-client/src/app/providers.tsx
+++ nextjs-client/src/app/providers.tsx
@@ -2,12 +2,15 @@
 
 import { AuthProvider } from "../contexts/AuthContext";
 import { ChakraProvider } from "@chakra-ui/react";
+import { ColorModeProvider } from "@/components/ui/color-mode";
 import { system } from "@/lib/theme";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ChakraProvider value={system}>
-      <AuthProvider>{children}</AuthProvider>
+      <ColorModeProvider>
+        <AuthProvider>{children}</AuthProvider>
+      </ColorModeProvider>
     </ChakraProvider>
   );
 }
